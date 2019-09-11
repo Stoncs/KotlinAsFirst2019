23a4e75403823193ee858b1eebcf47d1fcf63db5
Stoncs <andrej2002322@gmail.com>
2019-09-11 19:44:51 +0300

    first lesson

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -59,9 +59,8 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
-    return hours * 3600 + minutes * 60 + seconds
-}
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minutes * 60 + seconds
+
 
 
 
@@ -72,9 +71,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
-    return (sagenes * 48 + arshins * 16 + vershoks) * 0.04445
-}
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes * 48 + arshins * 16 + vershoks) * 0.04445
+
 
 /**
  * Тривиальная
@@ -82,9 +80,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
-    return deg * PI / 180 + min * PI / (180 * 60) + sec * PI / (180 * 60 * 60)
-}
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = deg * PI / 180 + min * PI / (180 * 60) + sec * PI / (180 * 60 * 60)
+
 
 /**
  * Тривиальная
@@ -92,9 +89,8 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
-    return sqrt(sqr(x2-x1) + sqr(y2-y1) )
-}
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr (x2 - x1) + sqr (y2 - y1) )
+
 
 /**
  * Простая
@@ -102,9 +98,8 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int {
-    return (number/ 100) % 10
-}
+fun thirdDigit(number: Int): Int = number / 100 % 10
+
 
 /**
  * Простая
@@ -113,9 +108,9 @@ fun thirdDigit(number: Int): Int {
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
-    return (hoursArrive - hoursDepart - 1) * 60 + 60 - minutesDepart + minutesArrive
-}
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+    (hoursArrive - hoursDepart - 1) * 60 + 60 - minutesDepart + minutesArrive
+
 
 /**
  * Простая
@@ -124,12 +119,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double {
-    val firstyear = initial + initial * percent * 0.01
-    val secondyear = firstyear + firstyear * percent * 0.01
-    val thirdyear = secondyear + secondyear * percent * 0.01
-    return  thirdyear
-}
+fun accountInThreeYears(initial: Int, percent: Int): Double = initial * (1 + percent * 0.01).pow(3)
+
 
 /**
  * Простая
@@ -139,7 +130,7 @@ fun accountInThreeYears(initial: Int, percent: Int): Double {
  */
 fun numberRevert(number: Int): Int {
     val first = number % 10
-    val second = (number / 10) % 10
+    val second = number / 10 % 10
     val third = number / 100
     val newnumber = first* 100 + second * 10 + third
     return newnumber
